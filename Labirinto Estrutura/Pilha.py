2b930a1b7c49bb65d467640d93e9c0fe4ec25a39
italoBrunoFS <italobrunofs1603@gmail.com>
2025-02-26 20:10:49 -0300

    Add files via upload

---
+++ Labirinto Estrutura/Pilha.py
@@ -0,0 +1,32 @@
+﻿class Pilha:
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.itens = []
+
+    def esta_vazia(self):
+        return len(self.itens) == 0
+
+    def esta_cheia(self):
+        return len(self.itens) == self.capacidade
+
+    def tamanho(self):
+        return len(self.itens)
+
+    def topo(self):
+        if self.esta_vazia():
+            raise IndexError("A pilha está vazia")
+        return self.itens[-1]
+
+    def push(self, item):
+        if self.esta_cheia():
+            raise OverflowError("A pilha está cheia")
+        self.itens.append(item)
+
+    def pop(self):
+        if self.esta_vazia():
+            raise IndexError("A pilha está vazia")
+        return self.itens.pop()
+
+
+
+
---
+++ Labirinto Estrutura/maze.py
@@ -0,0 +1,124 @@
+import pygame
+import numpy as np
+import csv
+import random
+import threading
+import time
+from Pilha import Pilha
+
+class Maze:
+
+    WALL = 0
+    HALL = 1
+    PLAYER = 2
+    PRIZE = 3
+    
+    def __init__(self):
+        self.M = None
+        pygame.init()
+        self.running = True
+
+    def load_from_csv(self, file_path: str):
+        with open(file_path, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            self.M = np.array([list(map(int, row)) for row in reader])
+            
+    def init_player(self):
+        while True:
+            posx = random.randint(2, 39)
+            posy = random.randint(2, 39)
+            if self.M[posx, posy] == Maze.HALL:
+                self.init_pos_player = (posx, posy)
+                break
+        
+        while True:
+            posx = random.randint(2, 39)
+            posy = random.randint(2, 39)
+            if self.M[posx, posy] == Maze.HALL:
+                self.M[posx, posy] = Maze.PRIZE
+                break
+
+    def find_prize(self, pos: (int, int)) -> bool:
+        if self.M[pos[0], pos[1]] == Maze.PRIZE:
+            return True
+        else:
+            return False
+        
+    def is_free(self, pos: (int, int)) -> bool:
+        if self.M[pos[0], pos[1]] in [Maze.HALL, Maze.PRIZE]:
+            return True
+        else:
+            return False
+        
+    def mov_player(self, pos: (int, int)) -> None:
+        if self.M[pos[0], pos[1]] == Maze.HALL:
+            self.M[pos[0], pos[1]] = Maze.PLAYER
+
+    def get_init_pos_player(self) -> (int, int):
+        return self.init_pos_player
+            
+    def run(self):
+        th = threading.Thread(target=self._display)
+        th.start()
+    
+    def _display(self, cell_size=15):
+        rows, cols = self.M.shape
+        width, height = cols * cell_size, rows * cell_size
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Labirinto")
+    
+        BLACK = (0, 0, 0)
+        GRAY = (192, 192, 192)
+        BLUE = (0, 0, 255)
+        GOLD = (255, 215, 0)
+    
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    pygame.quit()
+    
+            screen.fill(BLACK)
+    
+            for y in range(rows):
+                for x in range(cols):
+                    if self.M[y, x] == Maze.WALL:
+                        color = BLACK
+                    elif self.M[y, x] == Maze.HALL:
+                        color = GRAY
+                    elif self.M[y, x] == Maze.PLAYER:
+                        color = BLUE
+                    elif self.M[y, x] == Maze.PRIZE:
+                        color = GOLD
+                        
+                    pygame.draw.rect(screen, color, (x * cell_size, y * cell_size, cell_size, cell_size))
+    
+            pygame.display.flip()
+            
+    def backtracking(self):
+        pilha = Pilha(40 * 40) # Tamanho Max da Pilha
+        pos_inicial = self.get_init_pos_player()
+        pilha.push(pos_inicial)
+        
+        while not pilha.esta_vazia():
+            pos_atual = pilha.pop()
+            
+            if self.find_prize(pos_atual):
+                print("Caminho encontrado!")
+                return True
+            else:
+                self.M[pos_atual] = Maze.PLAYER
+                
+                vizinhos = [(pos_atual[0] - 1, pos_atual[1]),
+                            (pos_atual[0] + 1, pos_atual[1]),
+                            (pos_atual[0], pos_atual[1] - 1),
+                            (pos_atual[0], pos_atual[1] + 1)]
+                
+                for vizinho in vizinhos:
+                    if self.is_free(vizinho):
+                        pilha.push(vizinho)
+            time.sleep(0.05) # Velocidade do jogador
+        
+        print("Caminho não encontrado.")
+        return False
